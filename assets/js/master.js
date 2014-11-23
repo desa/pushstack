7d8f96a4eaeff562e9f73c53d40f90d55d4f88bf
Erica von Buelow <erica.vonbuelow@clever.com>
2014-11-23 14:39:55 -0800

    feat(server): add stylus compiler

---
+++ assets/js/master.js
@@ -0,0 +1,5 @@
+/**
+*
+* Pushstack - main script
+*
+**/
--- server.js
+++ server.js
@@ -4,10 +4,24 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var port = Number(process.env.PORT || 5000);
+var stylus = require('stylus');
 
-app.use(logfmt.requestLogger())
+app.use(logfmt.requestLogger());
 app.use(bodyParser());
-app.use('/', express.static(__dirname));
+
+app.use(stylus.middleware, {
+  src: __dirname + "/assets",
+  dest: __dirname + "/public",
+  compile: function(str, path) {
+    var styl = stylus(str)
+               .set('filename', path)
+               .import('_settings')
+               .use(require('axis-css'));
+    return styl;
+  }
+});
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, function() {
   console.log("Listening on " + port);
